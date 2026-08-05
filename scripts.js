502485d09ef740ce96db55354e890a22697e5ff9
jadynholbach-spec <jadyn.holbach@students.maestrocollege.edu>
2026-08-05 18:53:28 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,3 +2,14 @@ function toggleCaption(img) {
   const caption = img.nextElementSibling;
   caption.classList.toggle('show');
 }
+document.addEventListener("DOMContentLoaded", function() {
+  var modal = document.getElementById("welcome-modal");
+  var btn = document.getElementById("continue-btn");
+
+  if (modal && btn) {
+    modal.classList.remove("hidden");
+    btn.addEventListener("click", function() {
+      modal.classList.add("hidden");
+    });
+  }
+});
